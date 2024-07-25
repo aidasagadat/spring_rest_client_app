506ed0b0b98cd0760668f6e825250636ebb783b2
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-25 16:37:12 +0500

    Add configuration file

---
+++ src/main/java/com/zaurtregulov/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package com.zaurtregulov.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zaurtregulov.spring.rest")
+public class MyConfig {
+}
