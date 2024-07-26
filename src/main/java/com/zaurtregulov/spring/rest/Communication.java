9b4bc7123b0090d26adc8d0a8d68999e96715bc3
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-26 16:49:36 +0500

    Autowire resttemplate, add static URL, add methods to get all employees, get 1 employee by id, save or update employee and delete employee

--- src/main/java/com/zaurtregulov/spring/rest/Communication.java
+++ src/main/java/com/zaurtregulov/spring/rest/Communication.java
@@ -1,9 +1,78 @@
 package com.zaurtregulov.spring.rest;
 
+import com.zaurtregulov.spring.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Component
 public class Communication {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private String URL = "http://localhost:8080/spring_course_rest/api/employees";
+
+
+
+    public List<Employee> getALlEmployees(){
+
+        ResponseEntity<List<Employee>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<Employee>>() {});
+
+        List<Employee> allEmployees = responseEntity.getBody();
+
+        return allEmployees;
+
+    }
+
+
+    public Employee getEmployee(int id){
+        Employee employee = restTemplate.getForObject(URL + "/" + id, Employee.class);
+        return employee;
+    }
+
+
+
+    public void saveEmployee(Employee employee){
+        if(employee.getId() == 0){
+            restTemplate.postForEntity(URL, employee, String.class);
+            System.out.println("New emp added");
+        }
+        else{
+            restTemplate.put(URL, employee);
+            System.out.println("Emp updated");
+        }
+    }
+
+
+
+    public void deleteEmployee(int id){
+        restTemplate.delete(URL, id);
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
