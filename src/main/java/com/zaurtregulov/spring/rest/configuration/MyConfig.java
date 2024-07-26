0141d9d54e5a28195a1693549d3716a8147caf1f
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-26 16:53:08 +0500

    ...

--- src/main/java/com/zaurtregulov/spring/rest/configuration/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.zaurtregulov.spring.rest.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.zaurtregulov.spring.rest")
-public class MyConfig {
-}
