86000651d58fb505bd0d1f6a698d66b9b4800708
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-26 16:51:29 +0500

    Initialize Spring application context and retrieve Communication bean

--- src/main/java/com/zaurtregulov/spring/rest/App.java
+++ src/main/java/com/zaurtregulov/spring/rest/App.java
@@ -1,13 +1,18 @@
 package com.zaurtregulov.spring.rest;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import com.zaurtregulov.spring.rest.configuration.TheConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TheConfiguration.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+
+
     }
 }
